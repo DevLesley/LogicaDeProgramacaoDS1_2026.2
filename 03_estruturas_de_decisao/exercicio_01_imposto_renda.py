d92d55f74d23885a508a70eb110b7401bf4b250c
DevLesley <lesleylimaponte@gmail.com>
2026-09-17 14:45:59 -0300

    feat: exercicio 01

--- 03_estruturas_de_decisao/exercicio_01_imposto_renda.py
+++ 03_estruturas_de_decisao/exercicio_01_imposto_renda.py
@@ -13,3 +13,30 @@ Imprima "Isento" ou o valor total do imposto formatado com 2 casas decimais.
 """
 
 # TODO: Desenvolva o algoritmo abaixo:
+
+print("Seja bem vindo ao calculador de imposto de renda!!")
+print(" ")
+nome = input("Digite seu nome de usuário: ")
+salario = float(input("Digite seu salário: "))
+print(" ")
+
+
+if (salario <= 2000.00):
+    print(f"Parabéns {nome} você está isendo de impostos")
+elif (salario <= 3000.00 and salario > 2000):
+    taxa1 = (salario) * 0.08
+    valor = (taxa1) + salario
+    print(f"Você deve pagar um total de: {valor}R$ e {taxa1}R$ de imposto")
+    
+elif (salario <= 4500.00 and salario > 3000):
+    taxa1 = (1000) * 0.08
+    taxa2 = ((salario - 3000) * 0.18) + taxa1
+    valor = (taxa2) + salario
+    print(f"Você deve pagar um total de: {valor}R$ e {taxa2}R$ de imposto")
+elif (salario > 4500):
+    taxa1 = (1000) * 0.08
+    taxa2 = ((3000) * 0.18) + taxa1
+    taxa3 = ((salario - 4500) * 0.28) + taxa2
+    valor = (taxa3) + salario
+    print(f"Você deve pagar um total de: {valor}R$ e {taxa3}R$ de imposto")
+
