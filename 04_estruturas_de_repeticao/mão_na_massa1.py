b3959d392102f5cdaf3cde2ed8a7958d0c5c0fce
DevLesley <lesleylimaponte@gmail.com>
2026-09-24 22:18:30 -0300

    feat: inicio modulo 4

---
+++ 04_estruturas_de_repeticao/mão_na_massa1.py
@@ -0,0 +1,8 @@
+## 🛠️ Prática do Aluno (Mão na Massa)
+#Crie um programa que solicite um número inteiro ao usuário e imprima a **tabuada de multiplicação**
+#  desse número de 1 até 10 usando o laço `for`.
+
+numero = int(input("Digite um número inteiro para ver sua tabuada: "))
+for i in range(1, 11):
+    print(f"Tabuada do número {numero}:")
+    print()
